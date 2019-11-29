e38df8d0dd94b8f8987aeccd1383c7fbab552aae
Sa Eun Soo <tkdmstn0304@naver.com>
2019-11-29 16:43:29 +0900

    lab09

---
+++ lab09/maze.js
@@ -0,0 +1,36 @@
+/* CSE3026 : Web Application Development
+ * Lab 09 - Maze
+ */
+
+var loser = null;  // whether the user has hit a wall
+
+window.onload = function() {
+
+};
+
+// called when mouse enters the walls; 
+// signals the end of the game with a loss
+function overBoundary(event) {
+	
+}
+
+// called when mouse is clicked on Start div;
+// sets the maze back to its initial playable state
+function startClick() {
+
+}
+
+// called when mouse is on top of the End div.
+// signals the end of the game with a win
+function overEnd() {
+
+}
+
+// test for mouse being over document.body so that the player
+// can't cheat by going outside the maze
+function overBody(event) {
+
+}
+
+
+
